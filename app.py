44c18cb1e5bf776cd6e89a248f0d0119b79ae0f4
eclarasgsw <102331707+eclarasgsw@users.noreply.github.com>
2023-09-10 16:02:20 +0000

    working search

--- app.py
+++ app.py
@@ -3,25 +3,10 @@ import json
 
 app = Flask(__name__)
 
-JSON_DATA = {
-  "results": [{
-    'id': 1,
-    'adresse': 'Vadianstrasse 8, 9000 St.Gallen',
-    'waerme': 'Fernwärme',
-    'solar_potential': 'sehr gut'
-  }, {
-    'id': 2,
-    'adresse': 'Vadianstrasse 10, 9000 St.Gallen',
-    'waerme': 'Holzheizung',
-    'solar_potential': 'schlecht',
-  }]
-}
-
-
 @app.route("/")
 def home():
-  data = JSON_DATA["results"]
-  return render_template('home.html', data=data)
+  #data = JSON_DATA["results"]
+  return render_template('home.html')
 
 
 #https://www.digitalocean.com/community/tutorials/processing-incoming-request-data-in-flask#using-json-data
@@ -34,17 +19,18 @@ def search():
       data = json.load(json_file)
 
     searched_string = request.form.get('query')
+    print(searched_string)
     filtered_data = []
-
-    print('data: ', data)
 
     for record in data:
       if searched_string in record['adresse']:
         filtered_data.append(record)
         print("your word has been found and appended to filtered_data")
+      
+      else:
+        print("this string hasn't been found")
 
-    #for record in filtered_data:
-      #print(record)
+
     print(filtered_data)
     return render_template("search_results.html",
                            searched_string=searched_string,
@@ -54,12 +40,6 @@ def search():
     print("File 'data/input_data.json' not found.")
   except json.JSONDecodeError as e:
     print(f"JSON decoding error: {e}")
-
-  #data=JSON_DATA["results"]
-
-  #filtered_data = data.query(filterdata.adresse.like(searched_string)).all()
-
-  #return render_template("search_results.html", searched_string=searched_string, filtered_data=filtered_data)
 
 
 if __name__ == "__main__":
